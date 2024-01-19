d307d574a9af372f1e5d2f7ba811be23e10adbe5
SaltedEx <153088912+SaltedEx@users.noreply.github.com>
2024-01-19 17:18:20 +0800

    Save and Auto Save
    Saving done by PlayerPrefs

---
+++ PlayerPref Scripts/AutoSave.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoSave : MonoBehaviour
+{
+    float AutoTime = 14;
+    public GameObject player;
+    public Text alert;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AutoTime > 0)
+        {
+            AutoTime -= Time.deltaTime;
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("AutoX", player.transform.localPosition.x);
+            PlayerPrefs.SetFloat("AutoY", player.transform.localPosition.y);
+            PlayerPrefs.SetFloat("AutoZ", player.transform.localPosition.z);
+
+            alert.text = "Location Autosaved!";
+            AutoTime = 14;
+            Debug.Log(("X Axis: " + PlayerPrefs.GetFloat("AutoX") + " Y Axis: " + PlayerPrefs.GetFloat("AutoY") + " Z Axis: " + PlayerPrefs.GetFloat("AutoZ")));
+        }
+
+        if (AutoTime < 11)
+        {
+            alert.text = "Load from Last AutoSave";
+        }
+    }
+}
---
+++ PlayerPref Scripts/Save.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+    public GameObject player;
+    public void SavePosition()
+    {
+        PlayerPrefs.SetFloat("Y", player.transform.localPosition.y);
+        PlayerPrefs.SetFloat("X", player.transform.localPosition.x);
+        PlayerPrefs.SetFloat("Z", player.transform.localPosition.z);
+        Debug.Log(("X Axis: " + PlayerPrefs.GetFloat("X") + " Y Axis: " + PlayerPrefs.GetFloat("Y") + " Z Axis: " + PlayerPrefs.GetFloat("Z")));
+    }
+
+    public void LoadPosition()
+    {
+        player.transform.localPosition = new Vector3(PlayerPrefs.GetFloat("X"), PlayerPrefs.GetFloat("Y"), PlayerPrefs.GetFloat("Z"));
+    }
+
+    public void LoadAutoPos()
+    {
+        player.transform.localPosition = new Vector3(PlayerPrefs.GetFloat("AutoX"), PlayerPrefs.GetFloat("AutoY"), PlayerPrefs.GetFloat("AutoZ"));
+    }
+}
