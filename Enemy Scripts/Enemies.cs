bcb2969acd6e17371f5d559bba8af62698c1f212
SaltedEx <153088912+SaltedEx@users.noreply.github.com>
2024-01-19 17:15:32 +0800

    Enemy Scripts
    Basic AI of an Enemy and Spawning Points of the Enemy

---
+++ Enemy Scripts/Enemies.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemies : MonoBehaviour
+{
+
+    public Rigidbody enemyRigidBody;
+    public NavMeshAgent navMeshAgent;
+
+    public GameObject player;
+
+    public GameObject ambientBGM;
+    public GameObject conflictBGM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
+        ambientBGM = GameObject.Find("Ambient");
+        conflictBGM = GameObject.Find("Conflict");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(player.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ambientBGM.gameObject.SetActive(false);
+            conflictBGM.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ambientBGM.gameObject.SetActive(true);
+            conflictBGM.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Enemy Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyRef;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnEnemyMilee", 5f);
+    }
+
+    void SpawnEnemyMilee()
+    {
+        GameObject enemyMilee = (GameObject)Instantiate(enemyRef);
+        enemyMilee.transform.position = new Vector3(transform.position.x + 5, transform.position.y + 5, transform.position.z + 5);
+        Invoke("SpawnEnemyMilee", 5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
